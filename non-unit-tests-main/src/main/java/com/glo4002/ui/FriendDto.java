f7cd2945262591ada103e3e3f72b0a1f0e07320a
David Dufour <ada.david.dufour@gmail.com>
2020-11-14 12:15:35 -0500

    Added basic project structure.

---
+++ non-unit-tests-main/src/main/java/com/glo4002/ui/FriendDto.java
@@ -0,0 +1,14 @@
+package com.glo4002.ui;
+
+public class FriendDto {
+
+    private final String name;
+
+    public FriendDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
