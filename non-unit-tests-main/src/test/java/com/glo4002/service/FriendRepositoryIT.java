489e4da5dd1f5efaa7e85f1b9443779c9472905f
David Dufour <ada.david.dufour@gmail.com>
2020-11-14 12:31:42 -0500

    Added questions for IT.

---
+++ non-unit-tests-main/src/test/java/com/glo4002/service/FriendRepositoryIT.java
@@ -0,0 +1,10 @@
+package com.glo4002.service;
+
+public class FriendRepositoryIT {
+
+    /*
+        TODO: 2. Write necessary tests to validate all important database interactions.
+
+        The Solution can be found in the branch: solution/friend_repository_it
+     */
+}
