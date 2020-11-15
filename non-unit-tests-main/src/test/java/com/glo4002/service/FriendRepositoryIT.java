227f9d33735069b02d6fdda144947e16b2b6b4af
David Dufour <ada.david.dufour@gmail.com>
2020-11-15 01:44:47 -0500

    Fixed question.

--- non-unit-tests-main/src/test/java/com/glo4002/service/FriendRepositoryIT.java
+++ non-unit-tests-main/src/test/java/com/glo4002/service/FriendRepositoryIT.java
@@ -4,7 +4,7 @@ public class FriendRepositoryIT {
 
     /*
         TODO: 2. a) Write necessary non-unit tests to validate all important in-memory persistence behaviors.
-        TODO: b) How would tests differ in the case of an external database persistence?
+        TODO: b) How would non-unit tests differ when testing an external database persistence?
 
         The Solution can be found in the branch: solution/friend_repository_it
      */
